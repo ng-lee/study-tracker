0d9093164e6048077207c61c3d7774a5b5003e42
ng-lee <nhd5607@gmail.com>
2026-01-12 00:26:11 +0900

    feat: OAuth2 관련 에러 코드 추가

--- backend/src/main/kotlin/app/studytracker/global/error/ErrorCode.kt
+++ backend/src/main/kotlin/app/studytracker/global/error/ErrorCode.kt
@@ -7,5 +7,7 @@ enum class ErrorCode(
     val code: String,
     val message: String
 ) {
+    UNSUPPORTED_PROVIDER(HttpStatus.BAD_REQUEST, "A001","지원하지 않는 OAuth 프로바이더입니다."),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 오류가 발생했습니다."),
 }
