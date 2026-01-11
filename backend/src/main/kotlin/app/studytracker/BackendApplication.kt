33d4b58893a7fb82b7c1092b12dfa4b89ae81ea1
ng-lee <nhd5607@gmail.com>
2026-01-12 00:24:09 +0900

    config: Jpa Auditing 활성화

--- backend/src/main/kotlin/app/studytracker/BackendApplication.kt
+++ backend/src/main/kotlin/app/studytracker/BackendApplication.kt
@@ -2,8 +2,10 @@ package app.studytracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class BackendApplication
 
 fun main(args: Array<String>) {
